97e2430eb99dc0af2d80dd9d8d38779a53ab5822
apollo_jhn <japollo.ponteras@gmail.com>
2025-03-13 14:45:40 +0800

    TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-"use strict";
-const CONTENT_PATH = "content/";
+const CONTENT_PATH: string = "content/";
 const app = document.getElementById("app");
+
 window.onload = () => {
     loadContentOnApp("modeSelect");
-};
+}
+
 // Load Content on App Function
-function loadContentOnApp(htmlName) {
+function loadContentOnApp(htmlName: string) {
     if (app) { // Added null check here as well.
         loadContent(app, htmlName);
-    }
-    else {
+    } else {
         console.error("Element with ID 'app' not found.");
     }
 }
+
 // Load Content
-function loadContent(targetElement, html, basePath = CONTENT_PATH) {
+function loadContent(targetElement: HTMLElement, html: string, basePath = CONTENT_PATH) { // Corrected type here
     const path = basePath + html + ".html";
     targetElement.innerHTML = "";
     fetch(path)
